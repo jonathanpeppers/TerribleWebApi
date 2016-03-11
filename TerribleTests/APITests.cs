2ecff259bd8d92e07c00f1e1b753abaec2166b7d
Jonathan Peppers <jonathan.peppers@hitcents.com>
2016-03-11 14:00:08 -0600

    Added test project with 2 tests

---
+++ TerribleTests/APITests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Newtonsoft.Json;
+using TerribleWebApi.Models;
+using System.IO;
+
+namespace TerribleTests
+{
+    [TestFixture]
+    public class APITests
+    {
+        private const string BaseUrl = "http://turds.azurewebsites.net/api/";
+
+        [Test]
+        public async Task GetATurdNewWay()
+        {
+            var httpClient = new HttpClient();
+            string json = await httpClient.GetStringAsync(BaseUrl + "turd/1");
+            var turd = JsonConvert.DeserializeObject<Turd>(json);
+            Assert.IsNotNull(turd);
+        }
+
+        [Test]
+        public void GetATurdOldWay()
+        {
+            var request = HttpWebRequest.Create(BaseUrl + "turd/1") as HttpWebRequest;
+            using (var response = request.GetResponse() as HttpWebResponse)
+            using (var stream = response.GetResponseStream())
+            using (var reader = new StreamReader(stream))
+            {
+                string json = reader.ReadToEnd();
+                var turd = JsonConvert.DeserializeObject<Turd>(json);
+                Assert.IsNotNull(turd);
+            }
+        }
+    }
+}
